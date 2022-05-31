2f175d6c97fee190d5055ef0e575379a59400aed
Phan Duy <duyph.mangotech@gmail.com>
2022-05-31 18:19:10 +0700

    services and product

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    dots:false,
+    margin:40,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    autoPlaySpeed: 0,
+    smartSpeed:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
